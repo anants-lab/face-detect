172d0a6419c47d99f9e1b615d6357335344432fb
Anant Jain <anantjain@Anants-Macbook-Air.local>
2021-06-27 15:40:50 +0530

    new changes

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,53 @@ console.log("hello");
 
 const video = document.getElementById('video');
 
+class Queue
+{
+    // Array is used to implement a Queue
+    constructor()
+    {
+        this.items = [];
+    }
+    
+    enqueue(element)
+    {    
+      // adding element to the queue
+      this.items.push(element);
+    }
+
+    // dequeue function
+    dequeue()
+    {
+        // removing element from the queue
+        // returns underflow when called 
+        // on empty queue
+        if(this.isEmpty())
+            return "Underflow";
+        return this.items.shift();
+    }
+
+    // isEmpty function
+    isEmpty()
+    {
+        // return true if the queue is empty.
+        return this.items.length == 0;
+    }
+
+    printQueue()
+    {
+        var str = "";
+        for(var i = 0; i < this.items.length; i++)
+            str += this.items[i] +" ";
+        return str;
+    }
+
+    getLength(){
+      return this.items.length;
+  }
+
+}
+
+var queue = new Queue();
 
 
 Promise.all([
@@ -28,9 +75,62 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    //console.log(resizedDetections)
+    if(resizedDetections.length>0){
+    var expressions = resizedDetections[0].expressions.asSortedArray()
+    var exp = expressions[0].expression;
+    //console.log(queue.getLength())
+    if( queue.getLength()==10){
+      queue.dequeue();
+    }
+    queue.enqueue(exp)
+
+    //console.log(queue.printQueue())
+  
+    }
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
-})
+});
+
+$("#suggest-btn").click(function(){
+  let q=queue;
+  
+  var count = {
+    'surprised':0,
+    'disgusted':0,
+    'happy':0,
+    'sad':0,
+    'neutral':0,
+    'angry':0,
+    'fearful':0
+  };
+
+  let max=0,exp="";
+  while(!q.isEmpty()){
+    var curr_exp = q.dequeue()
+    count[curr_exp] += 1;
+    if(count[curr_exp]>max){
+      max=count[curr_exp];
+      exp=curr_exp;
+    }
+  }
+  //var exp = _.max(Object.keys(count), o => count[o]);
+
+  console.log(exp);
+  $.ajax({
+    type:"get",
+    url:`/expression/${exp}`,
+    success:function(data){
+      console.log(data.data);
+      $("#movie-image img").attr("src",data.data.poster);
+      $("#movie-desc-head").text(data.data.original_title);
+      $("#movie-desc-rating").text(data.data.vote_average);
+      $("#movie-desc-overview").text(data.data.overview);
+      $("#dropdown-content").css("display","block");
+      $("#suggest-btn button").text("Suggest Another Movie");
+    }
+  });
+});
--- index.js
+++ index.js
@@ -16,13 +16,48 @@ app.set("views", "./views");
 app.use(express.static("./assets"));
 
 
-Movie.find({genres:{$regex:"\W*((?i)Action|Adventure(?-i))\W*"}},function(err,movie){
-    console.log(movie);
-});
+
 
 app.get('/',function(req,res){
     res.render("index.ejs");
     return;
+});
+
+app.get('/expression/:exp',function(req,res){
+    if(req.xhr){
+        var regex = ""
+        switch(req.params.exp) {
+        case "surprised":
+            regex = "\W*((?i)Fantasy|Crime|Adventure|Mystery|Thriller(?-i))\W*"
+            break;
+        case "disgusted":
+            regex = "\W*((?i)Comedy|Romance|Science(?-i))\W*"
+            break;
+        case "happy":
+            regex = "\W*((?i)Animation|Family|Music(?-i))\W*"
+            break;
+        case "sad":
+            regex = "\W*((?i)Animation|Comedy|Drama(?-i))\W*"
+            break;
+        case "neutral":
+            regex = "\W*((?i)Action|War|Documentary|History (?-i))\W*"
+            break;
+        case "angry":
+            regex = "\W*((?i)Comedy|Romance|Adventure(?-i))\W*"
+            break;
+        case "fearful":
+            regex = "\W*((?i)Horror(?-i))\W*"
+            break;
+        }
+        Movie.find({genres:{$regex:`${regex}`}},function(err,movie){
+            let chosen=movie[Math.floor(Math.random() * movie.length+1)];
+            return res.status(200).json({
+                message:"Success",
+                data:chosen
+            });
+        });
+    }
+    
 });
 
 //Listens on port->8000
